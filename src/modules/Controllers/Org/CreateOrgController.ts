3dc6e91d8becc41ed2b5536113ac3bd54e16a26b
Breno Soriani <breno.soriani@gmail.com>
2025-08-08 16:27:12 -0300

    Infra

--- src/modules/Controllers/Org/CreateOrgController.ts
+++ src/modules/Controllers/Org/CreateOrgController.ts
@@ -1,8 +1,8 @@
 // src/modules/orgs/controllers/CreateOrgController.ts
 import { Request, Response } from "express";
-import { createOrgSchema } from "../dtos/create-org-dto";
-import { PrismaOrgRepository } from "../Prisma/OrgRepository";
-import { CreateOrgService } from "../services/CreateOrgService";
+import { createOrgSchema } from "../../dtos/create-org-dto";
+import { PrismaOrgRepository } from "../../Prisma/OrgRepository";
+import { CreateOrgService } from "../../services/CreateOrgService";
 
 export class CreateOrgController {
   async handle(req: Request, res: Response) {
